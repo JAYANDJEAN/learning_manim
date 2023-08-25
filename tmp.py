231ed3df19031d78430ecdfde37f2d47bc071fc7
Jay <54296281@qq.com>
2023-08-25 17:22:34 +0800

    Update tmp.py

--- tmp.py
+++ tmp.py
@@ -6,6 +6,8 @@ COLOR_ENVELOPE = GREY
 
 WIDTH_BACK = 3
 WIDTH_ENVELOPE = 1.5
+
+time_gap = 2
 PLANE = NumberPlane(
     background_line_style={
         "stroke_color": TEAL,
@@ -20,86 +22,131 @@ class MultiplyTwo(Scene):
         self.add(PLANE)
         time_create_dots = 2
         time_create_lines = 3
-        time_gap = 2
-        radius_circle = 3.5
+        time_draw_line = 3
+        radius_circle = 3
+        num_demo = 30
+        num_all = 200
+        radius_dot_demo = 0.05
+        radius_dot_all = 0.01
 
-        dots1 = self.get_dots(30, radius_circle, 0.05)
-        dots2 = self.get_dots(400, radius_circle, 0.03)
-        lines1 = self.get_lines(dots1, WIDTH_BACK)
-        lines2 = self.get_lines(dots2, WIDTH_ENVELOPE)
+        dots1, lines1 = self.get_lines(num_demo, radius_circle, radius_dot_demo, WIDTH_BACK)
+        dots2, lines2 = self.get_lines(num_all, radius_circle, radius_dot_all, WIDTH_ENVELOPE)
 
+        # 生成点
         self.play(Create(dots1, run_time=time_create_dots))
         self.wait(time_gap)
 
+        # 画线
         self.play(Create(lines1, run_time=time_create_lines))
         self.wait(time_gap)
 
+        # 线消失，点增多
         self.play(FadeOut(lines1))
         self.play(ReplacementTransform(dots1, dots2))
         self.wait(time_gap)
+
+        # 画线
         self.play(Create(lines2, run_time=time_create_lines * 2))
         self.play(FadeOut(dots2))
         self.wait(time_gap)
 
-    def get_dots(self, num_points, radius_circle, radius_dot):
-        circle = Circle(radius=radius_circle)
+        # 画极坐标图像
+        e = ValueTracker(0.01)
+        plane = PolarPlane().add_coordinates().move_to(DOWN)
+        graph = always_redraw(lambda: ParametricFunction(
+            lambda t: plane.polar_to_point(2 * (1 + np.sin(t)), t),
+            t_range=[0, e.get_value()], color=COLOR_BACK, stroke_width=WIDTH_BACK))
+        self.add(graph)
+        self.play(e.animate.set_value(2 * PI), run_time=time_draw_line, rate_func=linear)
+        self.wait(time_gap)
+
+    def get_lines(self, num_points, radius_circle, radius_dot, width):
         dots = VGroup()
+        lines = VGroup()
+        circle = Circle(radius=radius_circle)
         for i in range(num_points):
             dots.add(Dot(radius=radius_dot).move_to(
                 circle.point_at_angle(PI / 2 + i * 2 * PI / num_points)))
-        return dots
-
-    def get_lines(self, dots, width):
-        num_points = len(dots)
-        lines = VGroup()
         for i in range(num_points):
             lines.add(Line(dots[i], dots[(2 * i) % num_points],
                            color=COLOR_ENVELOPE,
                            stroke_width=width))
-        return lines
+        return dots, lines
 
 
 class CardioidByReflection(Scene):
+    # almost done
     def construct(self):
         self.add(PLANE)
         radius_circle = 3
         time_show = 4
-        time_create = 12
+        time_create = 10
+        time_draw_line = 4  # 画极坐标图像的时间
+        num_demo = 8
+        num_all = 128
+        dis_point = 4
 
         circle = Circle(radius=radius_circle, color=COLOR_BACK, stroke_width=WIDTH_BACK)
         dot = Dot(color=ORANGE).move_to(radius_circle * RIGHT)
 
+        # 画圆和点
         self.play(Create(circle), Create(dot))
-        self.wait()
+        self.wait(time_gap)
 
-        create_action_lines, lines = self.get_lines(radius_circle, 8, 0.3, WIDTH_BACK, time_show)
+        # 画实例光线
+        create_action_lines, lines = self.get_lines(radius_circle, num_demo, radius_circle, WIDTH_BACK, time_show)
         self.play(*create_action_lines)
-        self.wait(2)
+        self.wait(time_gap)
+
+        # 示例光线消失，画全部光线
         self.play(FadeOut(lines))
-        self.wait(2)
-        create_action_lines, lines = self.get_lines(radius_circle, 150, 1.0, WIDTH_ENVELOPE, time_create)
+        create_action_lines, lines = self.get_lines(radius_circle, num_all, radius_circle, WIDTH_ENVELOPE, time_create)
         self.play(*create_action_lines)
-        self.wait(2)
+        self.wait(time_gap)
 
-    def get_lines(self, radius_circle, num_points, radio_d, width, rt):
-        dots = VGroup()
+        # 画极坐标图像
+        e = ValueTracker(0.01)
+        plane = PolarPlane().add_coordinates().move_to(LEFT)
+        graph = always_redraw(lambda: ParametricFunction(
+            lambda t: plane.polar_to_point(2 * (1 + np.cos(t)), t),
+            t_range=[0, e.get_value()], color=COLOR_BACK, stroke_width=WIDTH_BACK))
+        self.add(graph)
+        self.play(e.animate.set_value(2 * PI), run_time=time_draw_line, rate_func=linear)
+        self.wait(time_gap)
+
+        # 全部光线和函数图像消失，移动点，画示例光线
+        self.play(FadeOut(graph, lines))
+        create_action_lines, lines = self.get_lines(radius_circle, num_demo, dis_point, WIDTH_BACK, time_show)
+        dot.generate_target()
+        dot.target.shift((dis_point - radius_circle) * RIGHT)
+        self.play(MoveToTarget(dot))
+        self.play(*create_action_lines)
+        self.wait(time_gap)
+
+        # 示例光线消失，画全部光线
+        self.play(FadeOut(lines))
+        create_action_lines, lines = self.get_lines(radius_circle, num_all, dis_point, WIDTH_ENVELOPE, time_create)
+        self.play(*create_action_lines)
+        self.wait(time_gap)
+
+    def get_lines(self, radius_circle, num_points, dis_point, width, rt):
         lines = VGroup()
-        create_action_lines = []
+        actions = []
+        radius_dot = 0.001
         circle = Circle(radius=radius_circle)
         for i in range(num_points):
             theta = i * 2 * PI / num_points
-            dots.add(Dot(radius=0.01).move_to(circle.point_at_angle(theta)))
-            pos_out = np.array([np.cos(theta * 2) * radio_d + (1 - radio_d) * np.cos(theta),
-                                np.sin(theta * 2) * radio_d + (1 - radio_d) * np.sin(theta),
-                                0]) * radius_circle
-            dots.add(Dot(radius=0.01).move_to(pos_out))
-
-        for i in range(0, num_points * 2, 2):
-            line_in = Line(dots[0], dots[i], color=COLOR_ENVELOPE, stroke_width=width)
-            line_out = Line(dots[i], dots[i + 1], color=COLOR_ENVELOPE, stroke_width=width)
-            create_action_lines.append(Create(VGroup(line_in, line_out), run_time=rt))
-            lines.add(line_in, line_out)
-        return create_action_lines, lines
+            phi = np.arctan(dis_point * np.sin(theta) / (radius_circle - dis_point * np.cos(theta)))
+            theta_new = PI - 2 * phi + theta
+            pos_out = np.array([np.cos(theta_new), np.sin(theta_new), 0]) * radius_circle
+            p0 = Dot(radius=radius_dot).move_to(RIGHT * dis_point)
+            p1 = Dot(radius=radius_dot).move_to(circle.point_at_angle(theta))
+            p2 = Dot(radius=radius_dot).move_to(pos_out)
+            line1 = Line(p0, p1, color=COLOR_ENVELOPE, stroke_width=width)
+            line2 = Line(p1, p2, color=COLOR_ENVELOPE, stroke_width=width)
+            actions.append(Create(VGroup(line1, line2), run_time=rt))
+            lines.add(line1, line2)
+        return actions, lines
 
 
 class CircleLine(Scene):
