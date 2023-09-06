a58bea0ae8bdfc68ab490bd55b2e0c1b1cc92d19
jay <54296281@qq.com>
2023-09-07 00:27:33 +0800

    new

--- 02_my_test/reflection.py
+++ 02_my_test/reflection.py
@@ -1,6 +1,54 @@
 from manim import *
 import numpy as np
 
+a = 5.0
+b = 4.0
+c = np.sqrt(np.square(a) - np.square(b))
+
+
+def get_slope_out(k_i, k_n):
+    return (2 * k_n - k_i + k_i * np.square(k_n)) / (1 + 2 * k_n * k_i - np.square(k_n))
+
+
+# ellipse
+def get_ellipse_y(x):
+    return np.sqrt((1 - np.square(x) / np.square(a)) * np.square(b))
+
+
+def get_ellipse_slope_n(x, y):
+    return y * np.square(a) / x / np.square(b)
+
+
+def get_ellipse_another_point(x0, y0, k):
+    A = (np.square(k) / np.square(b) + 1 / np.square(a))
+    B = (2 * k * y0 - 2 * np.square(k) * x0) / np.square(b)
+    C = (k * x0 - y0) * (k * x0 - y0) / np.square(b) - 1
+    x1 = (-B - np.sqrt(np.square(B) - 4 * A * C)) / 2 / A
+    x2 = (-B + np.sqrt(np.square(B) - 4 * A * C)) / 2 / A
+    x_p = x2 if np.abs(x1 - x0) < np.abs(x2 - x0) else x1
+    y_p = y0 + k * (x_p - x0)
+    return x_p, y_p
+
+
+def get_ellipse_reflections(x_ellipse, num):
+    y_ellipse = -get_ellipse_y(x_ellipse)
+    k_i = (y_ellipse - b) / x_ellipse
+    lines = VGroup()
+    lines.add(Line(np.array([0, b, 0]),
+                   np.array([x_ellipse, y_ellipse, 0])))
+    for i in range(num):
+        k_n = get_ellipse_slope_n(x_ellipse, y_ellipse)
+        k_o = get_slope_out(k_i, k_n)
+        x1, y1 = get_ellipse_another_point(x_ellipse, y_ellipse, k_o)
+
+        lines.add(Line(np.array([x_ellipse, y_ellipse, 0]),
+                       np.array([x1, y1, 0])))
+        x_ellipse = x1
+        y_ellipse = y1
+        k_i = k_o
+    lines.set_color_by_gradient(BLUE, YELLOW, RED)
+    return lines
+
 
 class Reflection(Scene):
     def construct(self):
@@ -8,37 +56,19 @@ class Reflection(Scene):
             background_line_style={
                 "stroke_color": TEAL,
                 "stroke_width": 3,
-                "stroke_opacity": 0.5
+                "stroke_opacity": 0.3
             }
         )
-        a = 6.0
-        b = 3.0
+
         ellipse = Ellipse(width=a * 2, height=b * 2, color=BLUE_B)
-        dot0 = Dot(radius=0.01, color=ORANGE).move_to(RIGHT * a)
-        x0 = -3
-        y0 = self.get_y(x0, a, b)
-        k_n = self.get_slope_n(x0, y0, a, b)
-        k_i = (y0 - 0) / (x0 - a)
-        k_o = self.get_slope_out(k_i, k_n)
-        x1, y1 = self.get_another_point(x0, y0, k_o, a, b)
-        line1 = Line(dot0, np.array([x0, y0, 0]))
-        line2 = Line(np.array([x0, y0, 0]), np.array([x1, y1, 0]))
+        dot_focus1 = Dot(color=ORANGE).move_to(RIGHT * c)
+        dot_focus2 = Dot(color=ORANGE).move_to(LEFT * c)
+        self.add(PLANE)
+        self.play(Create(ellipse), Create(dot_focus1), Create(dot_focus2))
+        self.wait()
 
-        self.add(PLANE, ellipse, line1, line2)
-
-    def get_slope_n(self, x, y, a, b):
-        return y * a * a / x / b / b
-
-    def get_slope_out(self, k_i, k_n):
-        return (2 * k_n + k_i * k_n * k_n - k_i) / (1 + 2 * k_n * k_i - k_n * k_n)
-
-    def get_y(self, x, a, b):
-        return np.sqrt((1 - x * x / a / a) * b * b)
-
-    def get_another_point(self, x0, y0, k, a, b):
-        A = (k * k / b / b + 1 / a / a)
-        B = (2 * k * y0 - 2 * k * k * x0) / b / b
-        C = (k * x0 - y0) * (k * x0 - y0) / b / b - 1
-        x1 = (-B - np.sqrt(B * B - 4 * A * C)) / 2 / A
-        y1 = self.get_y(x1, a, b)
-        return x1, y1
+        lines = get_ellipse_reflections(2.0, 70)
+        self.play(Create(lines), run_time=7)
+        self.wait()
+        self.play(FadeOut(lines))
+        self.wait()
--- 02_my_test/tmp.py
+++ 02_my_test/tmp.py
@@ -1,31 +1,24 @@
 from manim import *
 
-
-class EllipseReflection(Scene):
+class PlayAnimationsInGroup(Scene):
     def construct(self):
-        # 创建椭圆
-        ellipse = Ellipse(width=4, height=2, color=BLUE)
-        ellipse.move_to(ORIGIN)
+        # 创建一个圆和一个正方形
+        circle = Circle()
+        square = Square()
 
-        # 创建光源
-        light_source = Dot(color=YELLOW)
-        light_source.move_to(ellipse.point_from_proportion(0.3))  # 初始位置在椭圆内
+        # 创建动画对象
+        move_circle = circle.animate.shift(UP)
+        move_square = square.animate.shift(DOWN)
 
-        # 创建入射光线
-        incident_ray = Line(light_source.get_center(), ellipse.get_center(), color=YELLOW)
-        incident_ray.set_stroke(width=2)
+        # 创建动画组并设置总时长
+        group = AnimationGroup(
+            Create(circle),
+            Create(square),
+            move_circle,
+            move_square,
+            lag_ratio=0.5,  # 调整动画之间的延迟时间
+            run_time=3,     # 控制总时长
+        )
 
-        # 创建反射光线
-        reflected_ray = Line(ellipse.get_center(), 2 * RIGHT, color=GREEN)
-        reflected_ray.set_stroke(width=2)
-
-        # 显示椭圆、光源和入射光线
-        self.play(Create(ellipse))
-        self.play(Create(light_source))
-        self.play(Create(incident_ray))
-
-        # 添加反射动画
-        self.wait(0.5)
-        self.play(Transform(incident_ray, reflected_ray))
-
-        self.wait(1)
+        # 播放动画组
+        self.play(group)
