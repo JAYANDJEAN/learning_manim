a47f22a4c24d3ffd4333e69865cc01f71c8876e8
Jay <54296281@qq.com>
2023-09-07 18:54:39 +0800

    Update reflection.py

--- 02_my_test/reflection.py
+++ 02_my_test/reflection.py
@@ -2,8 +2,9 @@ from manim import *
 import numpy as np
 
 a = 5.0
-b = 4.0
+b = 3.0
 c = np.sqrt(np.square(a) - np.square(b))
+time_gap = 2
 
 
 def get_slope_out(k_i, k_n):
@@ -30,19 +31,20 @@ def get_ellipse_another_point(x0, y0, k):
     return x_p, y_p
 
 
-def get_ellipse_reflections(x_ellipse, num):
-    y_ellipse = -get_ellipse_y(x_ellipse)
+def get_ellipse_reflections(x_ellipse, y_ellipse, num):
     k_i = (y_ellipse - b) / x_ellipse
     lines = VGroup()
     lines.add(Line(np.array([0, b, 0]),
-                   np.array([x_ellipse, y_ellipse, 0])))
+                   np.array([x_ellipse, y_ellipse, 0]),
+                   stroke_width=1.5))
     for i in range(num):
         k_n = get_ellipse_slope_n(x_ellipse, y_ellipse)
         k_o = get_slope_out(k_i, k_n)
         x1, y1 = get_ellipse_another_point(x_ellipse, y_ellipse, k_o)
 
         lines.add(Line(np.array([x_ellipse, y_ellipse, 0]),
-                       np.array([x1, y1, 0])))
+                       np.array([x1, y1, 0]),
+                       stroke_width=1.5))
         x_ellipse = x1
         y_ellipse = y1
         k_i = k_o
@@ -52,23 +54,45 @@ def get_ellipse_reflections(x_ellipse, num):
 
 class Reflection(Scene):
     def construct(self):
+        # 片头
         PLANE = NumberPlane(
             background_line_style={
                 "stroke_color": TEAL,
-                "stroke_width": 3,
+                "stroke_width": 2,
                 "stroke_opacity": 0.3
             }
         )
+        self.add(PLANE)
+        title = Tex("Reflection in Ellipse").scale(3)
+        self.play(Write(title), run_time=3)
+        logo = MathTex(r"\mathbb{JAYANDJEAN}", fill_color="#343434").next_to(title, DOWN, buff=0.5).scale(2)
+        self.play(Write(logo), run_time=2)
+        self.wait()
+        self.play(FadeOut(title))
+        self.play(logo.animate.scale(0.4).move_to(RIGHT * 5.4 + UP * 3.2))
+        self.wait(2)
 
+        # 项目
         ellipse = Ellipse(width=a * 2, height=b * 2, color=BLUE_B)
         dot_focus1 = Dot(color=ORANGE).move_to(RIGHT * c)
         dot_focus2 = Dot(color=ORANGE).move_to(LEFT * c)
-        self.add(PLANE)
         self.play(Create(ellipse), Create(dot_focus1), Create(dot_focus2))
-        self.wait()
+        self.wait(time_gap)
 
-        lines = get_ellipse_reflections(2.0, 70)
-        self.play(Create(lines), run_time=7)
-        self.wait()
-        self.play(FadeOut(lines))
-        self.wait()
+        x_list = [2.0, 3.0, 4.0, 4.5, 4.95]
+        for x in x_list:
+            lines = get_ellipse_reflections(x, -get_ellipse_y(x), 100)
+            self.play(Create(lines), run_time=7)
+            self.wait(time_gap)
+            self.play(FadeOut(lines))
+            self.wait(time_gap)
+            if x == 4.95:
+                lines = get_ellipse_reflections(x, get_ellipse_y(x), 100)
+                self.play(Create(lines), run_time=7)
+                self.wait(time_gap)
+                self.play(FadeOut(lines))
+                self.wait(time_gap)
+        self.play(FadeOut(dot_focus2), FadeOut(dot_focus1))
+        self.wait(time_gap)
+        self.play(FadeOut(ellipse, shift=DOWN))
+        self.wait(time_gap)
