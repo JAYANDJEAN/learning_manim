2bb5fd7c1b907596aeedeef0f3f418b96f561850
fengyuan <54296281@qq.com>
2024-10-16 00:07:52 +0800

    mac

--- manim_protein/test.py
+++ manim_protein/test.py
@@ -1,15 +1,31 @@
 from Bio.PDB import PDBParser
+from manim import *
 
-# 创建解析器
-parser = PDBParser()
 
-# 解析结构
-structure = parser.get_structure('A0A4W3JAN5', 'data/A0A4W3JAN5.pdb')
+class PDBVisualization(Scene):
+    def construct(self):
+        parser = PDBParser()
+        structure = parser.get_structure('A0A4W3JAN5', 'data/A0A4W3JAN5.pdb')
+        atom_group = VGroup()
+        scala = 8
 
-# 遍历所有链和残基
-for model in structure:
-    for chain in model:
-        for residue in chain:
-            print(residue.get_resname())
-            for atom in residue:
-                print(atom.get_name(), atom.get_coord(), atom.get_bfactor(), atom.get_occupancy())
+        for model in structure:
+            for chain in model:
+                for residue in chain:
+                    print(residue.get_resname())
+                    for atom in residue:
+                        a = Sphere(radius=0.1)  # Each atom visualized as a small sphere
+                        a.move_to(atom.get_coord() / scala)  # Move to its (x, y, z) position
+                        a.set_color(BLUE)  # Set color of the atom
+                        atom_group.add(a)
+
+        self.add(atom_group)
+
+        # # Optional: Rotate to get a better 3D view
+        # self.play(Rotate(atom_group, angle=PI / 4, axis=UP))
+        #
+        # # Animate the scene
+        # self.wait()
+
+
+PDBVisualization().render()
