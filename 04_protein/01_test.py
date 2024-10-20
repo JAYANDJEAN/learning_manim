1b306a00edf7729adfd05b3e8bfa2d29f33be8e3
fengyuan <54296281@qq.com>
2024-10-20 20:52:47 +0800

    mac

--- 04_protein/01_test.py
+++ 04_protein/01_test.py
@@ -16,27 +16,3 @@ class Draw3DMorphine(ThreeDScene):
         self.stop_ambient_camera_rotation()
 
 
-class ProteinRibbonDiagram(ThreeDScene):
-    def construct(self):
-        # 使用3D场景
-        self.set_camera_orientation(phi=75 * DEGREES, theta=-45 * DEGREES)
-
-        # 示例点列表示蛋白质的主链
-        points = [
-            np.array([0, 0, 0]),
-            np.array([1, 2, 1]),
-            np.array([2, 1, 2]),
-            np.array([3, 3, 3]),
-            np.array([4, 0, 4]),
-        ]
-
-        # 创建曲线并将点平滑连接
-        ribbon = VMobject()
-        ribbon.set_points_smoothly(points)
-        ribbon.set_color(BLUE)
-
-        # 添加曲线
-        self.add(ribbon)
-
-
-# ProteinRibbonDiagram().render()
